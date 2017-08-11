5a507ce6d46a3cf4af12bf49d02bb8af8f8057b0
Daniel Jesús León Jardine <danjard@gmail.com>
2017-08-11 16:05:22 -0400

    Add files via upload

---
+++ dns.for
@@ -0,0 +1,23 @@
+$TTL 1D
+@       IN SOA kdns.strix.com.ve. root.strix.com.ve. (
+                                        201708; serial
+                                        1D; refresh
+                                        1H; retry
+                                        1W; expire
+                                        3H ; minimum
+)
+@                       IN      NS      kdns.strix.com.ve.
+kdns                    IN      A       192.168.0.112
+@                       IN      A       192.168.0.112
+drotaca-test            IN      A       192.168.0.21
+db1                     IN      A       192.168.0.71
+strixerp-local          IN      A       192.168.0.55
+amazonas                IN      A       192.168.0.66
+poraqui-api             IN      A       192.168.0.77 
+smbldap                 IN      A       192.168.0.114
+
+
+
+
+
+
